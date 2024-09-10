4cd62f3aab9ce73db76f72dd8e539d10f7945198
Jeffrey04 <mycyberpet@yahoo.com>
2024-09-10 22:17:26 +0800

    add database log to the application

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,20 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Query(models.Model):
+    query = models.CharField()
+    query_cleaned = models.CharField()
+
+    dns_public_result = models.CharField()
+    dns_public = models.CharField()
+
+    dns_isp_result = models.CharField()
+    dns_isp = models.CharField()
+
+    blocked = models.BooleanField()
+    different_ip = models.BooleanField()
+    measurement_url = models.CharField(blank=True)
+
+    creation_time = models.DateTimeField(default=datetime.now())
